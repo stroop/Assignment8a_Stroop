f27b279f8a29ad03e6a9bd917e0655a46e1f3c15
Jacob Stroop <israel941@gmail.com>
2016-02-07 16:31:10 -0500

    Initial Commit

---
+++ Assignment8a_Stroop/Assignment8a_Stroop/Assignment8a_Stroop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*
+Jacob Stroop
+CIS353 - Assignment 8a
+2/7/16
+*/
+
+namespace Assignment8a_Stroop
+{
+    static class Assignment8a_Stroop
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
