7817259252b0b5c02403220cd2047adab56e8510
Jacob Stroop <israel941@gmail.com>
2016-02-07 19:44:00 -0500

    Project Complete

--- Assignment8a_Stroop/Assignment8a_Stroop/Assignment8a_Stroop.cs
+++ Assignment8a_Stroop/Assignment8a_Stroop/Assignment8a_Stroop.cs
@@ -11,14 +11,46 @@ namespace Assignment8a_Stroop
 {
     class Assignment8 : Form
     {
-        private void btnClickMe(object sender, EventArgs e)
-        {
+        private Button btnClickMe;
+        private Button btnExit;
 
+        // constructor calls method to initialize form
+        public Assignment8()
+        {
+            InitializeComponent();
         }
 
-        private void btnExit(object sender, EventArgs e)
+        // initialization instructions
+        private void InitializeComponent()
         {
+            // btnClickMe config
+            this.btnClickMe = new System.Windows.Forms.Button(); // instantiate button
+            this.btnClickMe.Location = new System.Drawing.Point(15, 20); // button location
+            this.btnClickMe.Text = "Press Me"; // button text
+            this.btnClickMe.Click += new EventHandler(this.btnClickMe_Click); // button event handler
 
+            // btnExit config
+            this.btnExit = new System.Windows.Forms.Button(); // instantiate button
+            this.btnExit.Location = new System.Drawing.Point(15, 60); // button location
+            this.btnExit.Text = "Exit"; // button text
+            this.btnExit.Click += new EventHandler(this.btnExit_Click); // button event handler
+
+            // form config
+            this.ClientSize = new System.Drawing.Size(300, 150); // form size
+            this.Controls.Add(this.btnClickMe); // add button
+            this.Controls.Add(this.btnExit); // add button
+            this.Name = "Assignment8"; // form name
+            this.Text = "Assignment8"; // form title bar text
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Button Has Been Clicked.", "Click Me");
         }
     }
 
@@ -32,7 +64,7 @@ namespace Assignment8a_Stroop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Assignment8());
         }
     }
 }
